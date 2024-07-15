0b90f946ef24f70be932f268112451c88ec86209
Anatoly B <a.birtanov@takeoff-staff.ru>
2024-07-15 18:25:42 +0300

    init

---
+++ src/app.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import { MainPage } from "@/pages/main";
+
+export const App: FC = () => {
+  return <MainPage />;
+};
---
+++ src/modules/header/components/header/index.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.scss'
+
+export const Header = () => { 
+    return <header className={styles.header}>
+        <div className={styles.header__content}>asd</div>
+    </header>
+}
